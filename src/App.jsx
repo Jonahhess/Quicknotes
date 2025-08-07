20d55b7201a3841f38d433a6d0529bc5b7db38b1
jonahhess <jonahhessdev@gmail.com>
2025-08-07 17:27:47 +0300

    basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Note from "./Note";
+import Notelist from "./Notelist";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState([]);
 
+  function addNote(note) {
+    setNotes([note, ...notes]);
+  }
+
+  function removeNote(note) {
+    return setNotes(notes.filter((n) => n !== note));
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Note addNote={addNote}></Note>
+      <Notelist notes={notes} removeNote={removeNote}></Notelist>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
