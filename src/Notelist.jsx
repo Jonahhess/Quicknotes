61dc2eef7c48e099ad142d06d58eeb7fc3632836
jonahhess <jonahhessdev@gmail.com>
2025-08-14 09:47:49 +0300

    moved hover logic to css

--- src/Notelist.jsx
+++ src/Notelist.jsx
@@ -1,3 +1,19 @@
 export default function Notelist({ notes, removeNote }) {
-  return 0;
+  return notes.map((note, index) => (
+    <div key={index} id={`note-${index}`} className="note">
+      <button
+        id={`remove-${index}`}
+        onClick={() => removeNote(note)}
+        className="remove-btn"
+      >
+        x
+      </button>
+      <p className="text" id={`text-${index}`}>
+        {note.get(text)}
+      </p>
+      <p className="date" id={`date-${index}`}>
+        {note.get(date)}
+      </p>
+    </div>
+  ));
 }
