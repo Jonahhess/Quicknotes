767ae6dc45e2207fffc7cef9ddb6bd19b23ad9c4
jonahhess <jonahhessdev@gmail.com>
2025-08-15 17:36:47 +0300

    inline styles

--- src/Filters.jsx
+++ src/Filters.jsx
@@ -4,10 +4,21 @@ export default function Filters({ categoryFilters, setCategoryFilters }) {
   return (
     <div
       id="filters"
-      style={{ display: "flex", justifyContent: "space-evenly" }}
+      style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        border: "1px solid lightgrey",
+      }}
     >
+      <strong>Filter Notes</strong>
       {categories.map((c) => (
-        <div key={c}>
+        <div
+          key={c}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
           <label htmlFor={`checkbox-${c}`}>
             {c.charAt(0).toUpperCase() + c.slice(1)}
           </label>
@@ -15,6 +26,7 @@ export default function Filters({ categoryFilters, setCategoryFilters }) {
             type="checkbox"
             id={`checkbox-${c}`}
             defaultChecked
+            style={{ alignSelf: "center" }}
             onClick={() =>
               setCategoryFilters(
                 categoryFilters.includes(c)
