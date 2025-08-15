5c4e20d966463d006b13c4040fd5fbab331673c4
jonahhess <jonahhessdev@gmail.com>
2025-08-15 13:58:02 +0300

    passed updatedDate as prop

--- src/Notelist.jsx
+++ src/Notelist.jsx
@@ -3,12 +3,13 @@ import Note from "./Note";
 export default function Notelist({ notes, removeNote, openModal }) {
   return (
     <div id="note-list">
-      {notes?.map(({ title, text, date }, index) => (
+      {notes?.map(({ title, text, date, updatedDate }, index) => (
         <Note
           key={date}
           title={title}
           text={text}
           date={date}
+          updatedDate={updatedDate}
           index={index}
           removeNote={removeNote}
           openModal={openModal}
