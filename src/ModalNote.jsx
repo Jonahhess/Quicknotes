bae5aea3e99605e9cdcfac4b9ffc76cecdd9f2e2
jonahhess <jonahhessdev@gmail.com>
2025-08-15 13:17:20 +0300

    copy of newNote, will modify

---
+++ src/ModalNote.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function ModalNote({ addNoteToList }) {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  return (
+    <div id="new-note" className="floating">
+      <input
+        id="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Title"
+      ></input>
+      <div id="note-text">
+        <textarea
+          id="text"
+          placeholder="enter text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          rows="10"
+        ></textarea>
+      </div>
+      <button
+        id="submit"
+        onClick={() => {
+          addNoteToList({ title, text });
+          setText("");
+          setTitle("");
+        }}
+      >
+        Add Note
+      </button>
+    </div>
+  );
+}
