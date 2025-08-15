183f7f19e6697cfa5575df5f6c672bc9ac498bb4
jonahhess <jonahhessdev@gmail.com>
2025-08-15 16:11:41 +0300

    extracted filters logic

---
+++ src/Filters.jsx
@@ -0,0 +1,28 @@
+import categories from "./categories";
+
+export default function Filters(categoryFilters, setCategoryFilters) {
+  return (
+    <div
+      id="filters"
+      style={{ display: "flex", justifyContent: "space-evenly" }}
+    >
+      {categories.map((c) => (
+        <div key={c}>
+          <label htmlFor={`checkbox-${c}`}>{c || "uncategorized"}</label>
+          <input
+            type="checkbox"
+            id={`checkbox-${c}`}
+            defaultChecked
+            onClick={() =>
+              setCategoryFilters(
+                categoryFilters.includes(c)
+                  ? categoryFilters.filter((cat) => cat !== c)
+                  : [...categoryFilters, c]
+              )
+            }
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
