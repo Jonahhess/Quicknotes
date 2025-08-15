9186caac3ee1a5546604d161255d6558db9cb852
jonahhess <jonahhessdev@gmail.com>
2025-08-15 14:37:01 +0300

    passed category prop

--- src/Notelist.jsx
+++ src/Notelist.jsx
@@ -3,13 +3,14 @@ import Note from "./Note";
 export default function Notelist({ notes, removeNote, openModal }) {
   return (
     <div id="note-list">
-      {notes?.map(({ title, text, date, updatedDate }, index) => (
+      {notes?.map(({ title, text, date, updatedDate, category }, index) => (
         <Note
           key={date}
           title={title}
           text={text}
           date={date}
           updatedDate={updatedDate}
+          category={category}
           index={index}
           removeNote={removeNote}
           openModal={openModal}
