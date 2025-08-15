378b453ddc0eecebaec0dfeddc6182ecf2a507a2
jonahhess <jonahhessdev@gmail.com>
2025-08-15 16:32:22 +0300

    first formatting, used better default

--- src/NewNote.jsx
+++ src/NewNote.jsx
@@ -4,7 +4,7 @@ import categories from "./categories";
 export default function NewNote({ addNoteToList }) {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState("uncategorized");
 
   return (
     <div id="new-note" className="floating">
@@ -22,21 +22,21 @@ export default function NewNote({ addNoteToList }) {
           onChange={(e) => setText(e.target.value)}
           rows="10"
         ></textarea>
-        <div id="category-container">
-          <label htmlFor="categories">Category</label>
-          <select
-            name="categories"
-            id="categories"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          >
-            {categories.map((c) => (
-              <option value={c}>
-                {c.charAt(0).toUpperCase() + c.slice(1)}
-              </option>
-            ))}
-          </select>
-        </div>
+      </div>
+      <div id="category-container">
+        <label htmlFor="categories">Category</label>
+        <select
+          name="categories"
+          id="categories"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          {categories.map((c) => (
+            <option key={c} value={c}>
+              {c.charAt(0).toUpperCase() + c.slice(1)}
+            </option>
+          ))}
+        </select>
       </div>
       <button
         id="submit"
