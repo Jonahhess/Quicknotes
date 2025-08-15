5034c17f3467c72565a6e25661a4b9dffe98b0b5
jonahhess <jonahhessdev@gmail.com>
2025-08-15 14:36:28 +0300

    added controlled component category

--- src/NewNote.jsx
+++ src/NewNote.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function NewNote({ addNoteToList }) {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
+  const [category, setCategory] = useState("");
 
   return (
     <div id="new-note" className="floating">
@@ -20,11 +21,26 @@ export default function NewNote({ addNoteToList }) {
           onChange={(e) => setText(e.target.value)}
           rows="10"
         ></textarea>
+        <div id="category-container">
+          <label htmlFor="categories">Category</label>
+          <select
+            name="categories"
+            id="categories"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value=""></option>
+            <option value="personal">Personal</option>
+            <option value="work">Work</option>
+            <option value="reminder">Reminder</option>
+            <option value="todo">Todo</option>
+          </select>
+        </div>
       </div>
       <button
         id="submit"
         onClick={() => {
-          addNoteToList({ title, text });
+          addNoteToList({ title, text, category });
           setText("");
           setTitle("");
         }}
