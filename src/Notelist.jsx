75dfb020254ac3fd4aa9fb6ba14fb39ce0fce16c
jonahhess <jonahhessdev@gmail.com>
2025-08-15 13:06:27 +0300

    simple container passing props

--- src/Notelist.jsx
+++ src/Notelist.jsx
@@ -1,43 +1,18 @@
-import { useState } from "react";
+import Note from "./Note";
 
 export default function Notelist({ notes, removeNote, openModal }) {
-  const [hoveredIndex, setHoveredIndex] = useState(null);
   return (
     <div id="note-list">
       {notes?.map(({ title, text, date }, index) => (
-        <div
+        <Note
           key={date}
-          id={`note-${index}`}
-          className="note floating"
-          onMouseEnter={() => setHoveredIndex(index)}
-          onMouseLeave={() => setHoveredIndex(null)}
-          onClick={openModal}
-        >
-          <p id={`date-${index}`} className="date">
-            {new Date(date).toLocaleString("default", {
-              month: "short",
-              day: "numeric",
-              hour: "numeric",
-              minute: "numeric",
-            })}
-          </p>
-          <h1 className="title" id={`text-${index}`}>
-            {title}
-          </h1>
-          <p className="text" id={`text-${index}`}>
-            {text}
-          </p>
-          <button
-            id={`remove-${index}`}
-            onClick={() => removeNote(date)}
-            className="remove-btn"
-            style={{
-              visibility: hoveredIndex === index ? "visible" : "hidden",
-            }}
-          >
-            X
-          </button>
-        </div>
+          title={title}
+          text={text}
+          date={date}
+          index={index}
+          removeNote={removeNote}
+          openModal={openModal}
+        />
       ))}
     </div>
   );
