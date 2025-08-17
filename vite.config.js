d1f2ef113f1bcd65719e59ba748ff9a57cfa5550
jonahhess <jonahhessdev@gmail.com>
2025-08-17 08:59:48 +0300

    another try

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/jonahhess.github.io/",
+  base: "/Quicknotes/",
 });
